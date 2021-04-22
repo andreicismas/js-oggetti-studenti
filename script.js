3b033a16fd47fe0c83f7aa5f6036b2bdf199b3fa
andreicismas <andrei_cismas@yahoo.it>
2021-04-22 16:48:12 +0200

    work on output html

--- script.js
+++ script.js
@@ -9,12 +9,9 @@
 
 
 
-// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 
 
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 
-// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. -->
 
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 
@@ -28,52 +25,77 @@ var studentDiv = document.getElementById("proprieta-oggeto")
 // alternativa senza appici 
 
 // var student ={
-//     nome: "andrei",
-//     cognome: "cismasu",
-//     eta:33
-// }
+    //     nome: "andrei",
+    //     cognome: "cismasu",
+    //     eta:33
+    // }
+    
+    // il ciclo della variabile "object" student 
+    
+    // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+    for (var credenziali in student) {
+        // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+        console.log("lo studente è " + credenziali + ": " + student
+        [credenziali]);
+        
+    }
+    
+    studentDiv.innerHTML=student
 
-// il ciclo della variabile "object" student 
+    // Creare un array di oggetti di studenti.
+    
+    // array vuoto
+    
+    var listStudents = [];
+    
+    // nuovi objects da pushare in array
+    
+    var allievo1 = {};
+    
+    allievo1.nome = "Coco";
+    allievo1.cognome = "Mimmo";
+    allievo1.eta = 21;
+    listStudents.push(allievo1)
+    
+    var allievo2 = {};
+    
+    allievo1.nome = "Marco";
+    allievo1.cognome = "Milvio";
+    allievo1.eta = 31;
+    listStudents.push(allievo1)
+    
+    
+    var allievo3 = {};
+    
+    allievo1.nome = "Bebe";
+    allievo1.cognome = "Frank";
+    allievo1.eta = 34;
+listStudents.push(allievo1)
 
-for (var credenziali in student) {
-    console.log("lo studente è " + credenziali + ": " + student
-    [credenziali]);
+
+
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. -->
+
+//creo un nuovo oggetto da pushare in listStudents
+var ultimoArrivato = {}
+
+
+//push all'interno  array  listStudents
+listStudents.push(ultimoArrivato);
+console.log(listStudents)
+
+// i prompt dati ultimo arrivato 
+var nomeInput = prompt("inserisci il nome utente");
+var cognomeInput = prompt("inserisci il cognome utente");
+
+while (isNaN(etaUltimoArrivato)) {
+    var etaUltimoArrivato = parseInt(prompt("inserisci età utente"));
 }
 
+ultimoArrivato.nome = nomeInput;
+ultimoArrivato.cognome = cognomeInput;
+ultimoArrivato.eta = etaUltimoArrivato;
 
-// Creare un array di oggetti di studenti.
+console.log("ultimo ogg arrivato e :", ultimoArrivato);
 
-// array vuoto
-
-var listStudents = [];
-
-// nuovi objects da pushare in array
-
-var allievo1 = {};
-
-allievo1.nome = "Coco";
-allievo1.cognome = "Mimmo";
-allievo1.eta = 21;
-listStudents.push(allievo1)
-
-var allievo2 = {};
-
-allievo1.nome = "Marco";
-allievo1.cognome = "Milvio";
-allievo1.eta = 31;
-listStudents.push(allievo1)
-
-
-var allievo3 = {};
-
-allievo1.nome = "Bebe";
-allievo1.cognome = "Frank";
-allievo1.eta = 34;
-listStudents.push(allievo1)
-
-// ciclo for per stampare la lunghezza del mio array"listStudents"
-
-for(var i =0; i<listStudents.length; i++){
-    console.log(listStudents[i])
-}
 
